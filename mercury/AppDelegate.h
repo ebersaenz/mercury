acf88ba2b12929aa6b6408b7d272e445ae383b75
ebersaenz <ebersaenz@yahoo.com>
2025-07-25 15:29:21 -0700

    Restarting with objective-c based project

---
+++ mercury/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  mercury
+//
+//  Created by Eber Saenz on 7/25/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ mercury/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  mercury
+//
+//  Created by Eber Saenz on 7/25/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
